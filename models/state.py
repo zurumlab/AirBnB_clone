9687ad58cf108766f337796a7074fb6ce9dfe28b
zurumlab <akanduj59@yahoo.com>
2023-02-09 00:26:44 -0800

    added console.py file, created a model class, created the test files

---
+++ models/state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+""" holds class State"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Representation of state """
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)#!/usr/bin/python
+""" holds class State"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Representation of state """
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
